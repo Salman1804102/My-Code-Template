1c047b1f02ae60afa2f81e351ee3024b97e34e77
Salman Farsi <62229105+Salman1804102@users.noreply.github.com>
2023-05-14 23:50:57 +0600

    Update demo.cpp

--- Miscellaneous /demo.cpp
+++ Miscellaneous /demo.cpp
@@ -1,1 +1,15 @@
-
+string deci_to_bin(int n)
+{
+	string res = "";
+	while (n)
+	{
+		int r = n % 2;
+		res += (r + '0');
+		n /= 2;
+	}
+	int sz = 31 - (int)res.size();
+	for (int i = 1; i <= sz; i++) res += '0';
+	// debug(res);
+	reverse(res.begin(), res.end());
+	return res;
+}
