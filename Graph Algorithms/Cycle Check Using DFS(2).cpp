e66ce83acc4f08aa5ac52620df6c992dac1a489b
Salman Farsi <62229105+Salman1804102@users.noreply.github.com>
2023-05-20 01:07:58 +0600

    Update Cycle Check Using DFS(2).cpp

--- Graph Algorithms/Cycle Check Using DFS(2).cpp
+++ Graph Algorithms/Cycle Check Using DFS(2).cpp
@@ -1,7 +1,9 @@
-bool dfs(int pos, int pre) {
+bool dfs(int pos, int pre) 
+{
     if(vis[pos]) return true;
     vis[pos] = 1;
-    for(auto it : v[pos]) {
+    for(auto it : v[pos]) 
+    {
         if(it == pre) continue;
         if(dfs(it, pos)) return true;
     }
