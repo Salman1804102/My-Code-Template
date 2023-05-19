cc36770bf6fa0794dedc9358aceef8bba47f4326
Salman Farsi <62229105+Salman1804102@users.noreply.github.com>
2023-05-20 01:05:05 +0600

    Update Cycle Check Using DFS.cpp

--- Graph Algorithms/Cycle Check Using DFS.cpp
+++ Graph Algorithms/Cycle Check Using DFS.cpp
@@ -1,49 +1,100 @@
+//Codeforces - Round Dance, Link : https://codeforces.com/contest/1833/problem/E
+
 #include<bits/stdc++.h>
-#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+#define fastio  ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define print(v) for(auto it : v) cout << it << ' '; cout << endl;
+#define valid(i, j, n, m) (i >= 0 && i < n && j >= 0 && j < m)
+#define all(x) (x).begin(), (x).end()
+#define revall(x) (x).rbegin(), (x).rend()
+#define kase(i) cout << "Case " << i << ": ";
+#define debug(x) cout << #x << "--> " << x << endl;
+#define int long long
+#define pii pair<int, int>
+#define ppii pair<pii, int>
+#define fi first
+#define se second
+#define inf 1e18
+#define set(x, i) (x | (1 << i))
+#define check(x, i) (x & (1 << i))
+#define lcm(a, b) ((a  * b) / __gcd(a, b))
+
 using namespace std;
-int n, m, vis[101], parent[101];
-vector<int>graph[101];
-bool BFS(int src)
+using namespace __gnu_pbds;
+
+template <typename T> using o_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>; // find_by_order and order_of_key
+template <typename T, typename R> using o_map = tree<T, R, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T> using o_mset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>; // find_by_order and order_of_key
+
+const int mod = 1e9 + 7;
+const int N = 2e5 + 5;
+vector<int>graph[N];
+int vis[N], iscycle = 0;
+void dfs(int src, int parent)
 {
-	for (int i = 1; i <= n; i += 1) vis[i] = 0, parent[i] = -1;
-	vis[src] = true;
-	queue<int>q;
-	q.push(src);
-	while (!q.empty())
-	{
-		int u = q.front(); q.pop();
-		for (auto v : graph[u])
-		{
-			if (vis[v] == 0)
-			{
-				vis[v] = true;
-				parent[v] = u;
-				q.push(v);
-			}
-			else
-			{
-				if (v != parent[u]) return true;
-			}
-		}
-	}
-	return false;
+    if (vis[src] == 2) return;
+
+    if (vis[src] == 1)
+    {
+        iscycle = 1;
+        return;
+    }
+
+    vis[src] = 1;
+    for (auto it : graph[src])
+    {
+        if (it != parent) dfs(it, src);
+    }
+    vis[src] = 2;
 }
-int main()
+main()
 {
-	fastio;
+    fastio;
 #ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
 #endif
-	cin >> n >> m;
-	for (int i = 0; i < m; i++)
-	{
-		int u, v; cin >> u >> v;
-		graph[u].push_back(v);
-		graph[v].push_back(u);
-	}
-	bool cycle = BFS(1);
-	if (cycle) cout << "Yes, the graph contains the cycle" << endl;
-	else cout << "The graph doesn't have any cycle" << endl;
-	return 0;
+    int tc; cin >> tc;
+    while (tc--)
+    {
+        int n; cin >> n;
+        map<pii, int>mp;
+        for (int i = 1; i <= n; i++)
+        {
+            int u = i;
+            int v; cin >> v;
+            if (v < u) swap(v, u);
+            if (mp[pii(v, u)] == 0)
+            {
+                graph[u].push_back(v);
+                graph[v].push_back(u);
+                mp[pii(u, v)] = 1;
+            }
+        }
+        int component = 0, cycle = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            if (vis[i] == 0)
+            {
+                component += 1;
+                dfs(i, -1);
+                cycle += iscycle;
+                iscycle = 0;
+            }
+        }
+        if (cycle)
+        {
+            if (cycle != component) cout << cycle + 1 << ' ' << component << endl;
+            else cout << component << ' ' << component << endl;
+        }
+        else cout << 1 << ' ' << component << endl;
+        for (int i = 0; i <= n; i++)
+        {
+            graph[i].clear();
+            vis[i] = 0;
+        }
+    }
 }
